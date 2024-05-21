33e8ad54b60a05e0eebd01f760ff88d3cc43f75d
happiipark <124240328+happiipark@users.noreply.github.com>
2024-05-22 02:18:41 +0900

    컴포넌트 최적화
    컴포넌트 최적화 방법

--- React/todo-app/src/App.js
+++ React/todo-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useReducer, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -15,8 +15,26 @@ function createBulkTodos() {
   return array;
 }
 
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT': // 새로 추가
+      // { type : "INSERT", todo:{id : 1, text : "todo", checked : false }}
+      return todos.concat(action.todo);
+    case 'REMOVE': // 제거
+      // { type : "REMOVE" , id : 1 }
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE': // 토글
+      // { type : "TOGGLE", id : 1 }
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
 const App = () => {
-  const [todos, setTodos] = useState(createBulkTodos);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
 
   // 고윳값으로 사용될 id
   // ref를 사용하여 변수 담기
@@ -30,7 +48,8 @@ const App = () => {
         checked: false,
       };
       // setTodos(todos.concat(todo));
-      setTodos((todos) => todos.concat(todo));
+      // setTodos((todos) => todos.concat(todo));
+      dispatch({ type: 'INSERT', todo });
       nextId.current += 1; // nextId 1씩 더하기
     },
     // [todos],
@@ -40,7 +59,8 @@ const App = () => {
   const onRemove = useCallback(
     (id) => {
       // setTodos(todos.filter((todo) => todo.id !== id));
-      setTodos((todos) => todos.filter((todo) => todo.id !== id));
+      // setTodos((todos) => todos.filter((todo) => todo.id !== id));
+      dispatch({ type: 'REMOVE', id });
     },
     // [todos],
     [],
@@ -53,11 +73,12 @@ const App = () => {
       //     todo.id === id ? { ...todo, checked: !todo.checked } : todo,
       //   ),
       // );
-      setTodos((todos) =>
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
+      // setTodos((todos) =>
+      //   todos.map((todo) =>
+      //     todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      //   ),
+      // );
+      dispatch({ type: 'TOGGLE', id });
     },
     // [todos],
     [],
